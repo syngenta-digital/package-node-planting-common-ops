e73e898131578d05ab14729f6c9478880aaf247b
Vansham Aggarwal <vansham.aggarwal@syngenta.com>
2024-02-22 14:52:19 +0530

    CWPT-714 | fix: changes in the class definition

--- src/modules/secret-manager-client/index.ts
+++ src/modules/secret-manager-client/index.ts
@@ -73,10 +73,10 @@ export class SecretManagerPlantingClient implements ISecretManagerPlantingClient
 		}
 	}
 
-	public generateSecretName(name: string) {
+	static generateSecretName(name: string) {
 		const env = process.env.NODE_ENV ?? process.env.ENV;
 		const effectiveEnv = env === 'production' ? 'prod' : env === 'staging' ? 'uat' : env;
 		return `planting/${effectiveEnv}/${name}`;
 	}
-
+  
 }
