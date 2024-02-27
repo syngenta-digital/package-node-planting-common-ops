04c18feab86cfe8cb073e6161bb37d0ec52e83f3
Vansham Aggarwal <vansham.aggarwal@syngenta.com>
2024-02-27 12:57:25 +0530

    CWPT-746 | improvement: Some improvements in the package
    - Implemented Prettier
    - More linting rules
    - Included the .env files in the dist folder.
    - Removed src folder during build and packaging
    - Error handling in the `generateName` function

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,54 @@
 module.exports = {
-	'parser': '@typescript-eslint/parser',
-	'env': {
-		'browser': true,
-		'node': true,
-		'es2021': true,
-		'jest': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/eslint-recommended',
-	],
-	'overrides': [
-	],
-	'parserOptions': {
-		'ecmaVersion': 2018,
-		'sourceType': 'module',
-		'ecmaFeatures': {
-			'modules': true
-		}
-	},
-	'plugins': [
-		'@typescript-eslint'
-	],
-	'settings': {
-	},
-	'rules': {
-		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-		// e.g. '@typescript-eslint/explicit-function-return-type': 'off',
-		'brace-style': [ 'error', 'allman', { 'allowSingleLine': true } ],
-		'@typescript-eslint/explicit-member-accessibility': ['error', { overrides: { parameterProperties: 'off' } }],
+  'parser': '@typescript-eslint/parser',
+  'env': {
+    'browser': true,
+    'node': true,
+    'es2021': true,
+    'jest': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    // 'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'modules': true
+    }
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'prettier'
+  ],
+  'settings': {
+  },
+  'rules': {
+    '@typescript-eslint/explicit-member-accessibility': ['error', { overrides: { parameterProperties: 'off' } }],
     '@typescript-eslint/explicit-member-accessibility': 'off',
-		'indent': [ 'error', 'tab' ],
-		'no-mixed-spaces-and-tabs': 'error',
-		'no-trailing-spaces': 'off',
-		'no-unused-vars': 'off',
-		'react/prop-types': 'off',
-		'semi': [ 'error', 'always' ],
-		'quotes': [ 'error', 'single' ],
-    'brace-style': ['error','stroustrup']
-	}
+    'indent': ['error', 2],
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-trailing-spaces': 'off',
+    'no-unused-vars': [
+      'error',
+      { 'vars': 'all', varsIgnorePattern: "_", args: "none" }
+    ],
+    'react/prop-types': 'off',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single', {allowTemplateLiterals: true}],
+    'brace-style': ['error', '1tbs'],
+    'max-len': [
+      'error',
+      {
+        code: 80, // Adjust based on your preferred line length
+        ignoreTemplateLiterals: true,
+        ignoreComments: true,
+        ignorePattern: '^import\\s.+\\sfrom\\s.+;$', // Ignore import statements
+      },
+    ],
+  }
 };
